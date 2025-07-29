ded62adf7a20f081c6eb31eabbbed186c26c282e
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-07-29 20:38:23 +0300

    Character structure change

--- models/Character.js
+++ models/Character.js
@@ -7,13 +7,28 @@ class Character {
 		this.defense = defense;
 	}
 
+	// positive effects will be positive number, negative effect negative number
+	getTotalDefense(effect = 0) {
+		const totalDefense = this.defense + effect;
+		return totalDefense > 0 ? totalDefense : 0;
+	}
+
+	getTotalAttack(effect = 0) {
+		const totalAttack = this.attack + effect;
+		return totalAttack > 0 ? totalAttack : 0;
+	}
+
 	attackTarget(target) {
 		const damage = this.attack - target.defense;
 		// make sure damage can't be less then zero
 		const actualDamage = damage > 0 ? damage : 0;
-		target.health -= actualDamage;
-		console.log(
-			`${this.name} attacks ${target.name} and deals ${actualDamage} damage`
-		);
+		// make sure target health can't be less then zero
+		target.health = Math.max(0, target.health - actualDamage);
+		return {
+			attacker: this.name,
+			target: target.name,
+			damage: actualDamage,
+			targetHealth: target.health,
+		};
 	}
 }
