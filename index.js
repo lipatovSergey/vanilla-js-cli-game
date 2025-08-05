b45fb865e6e4b29083394a6279195c8309b7f8ea
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-08-05 06:02:40 +0300

    loop added

--- index.js
+++ index.js
@@ -3,35 +3,36 @@ import { Warrior, Mage, Rogue } from "./models/Character.js";
 import readline from "readline";
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const classMap = {
-	Warrior,
-	Mage,
-	Rogue,
+  Warrior,
+  Mage,
+  Rogue,
 };
 
-classMap.map((cls, index) => {
-	console.log(cls);
-});
 const classes = ["Warrior", "Mage", "Rogue"];
-console.log("Choose your class:");
-classes.forEach((cls, index) => {
-	console.log(`${index + 1}. ${cls}`);
-});
+function askForClass() {
+  console.log("Choose your class:");
+  classes.forEach((cls, index) => {
+    console.log(`${index + 1}. ${cls}`);
+  });
 
-rl.question("Enter the number of your class: ", classChoice => {
-	const choiceIndex = parseInt(classChoice) - 1;
-	if (choiceIndex < 0 || choiceIndex > classes.length) {
-		console.log("Invalid choice");
-		rl.close();
-		return;
-	}
-	const selectedClass = classes[choiceIndex];
-	rl.question("Enter your character's name: ", characterName => {
-		console.log(`You created a ${selectedClass} named ${characterName}`);
-		rl.close();
-	});
-});
+  rl.question("Enter the number of your class: ", (classChoice) => {
+    const choiceIndex = parseInt(classChoice) - 1;
+    if (choiceIndex < 0 || choiceIndex > classes.length - 1) {
+      console.log("Invalid choice");
+      askForClass();
+      return;
+    }
+    const selectedClass = classes[choiceIndex];
+    rl.question("Enter your character's name: ", (characterName) => {
+      console.log(`You created a ${selectedClass} named ${characterName}`);
+      rl.close();
+    });
+  });
+}
+
+askForClass();
